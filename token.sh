805adc907c55b489fad56bfa7dfa1a164f8ecc46
Anuj2140 <161712659+Anuj2140@users.noreply.github.com>
2024-08-04 19:46:36 +0530

    Create token.sh

---
+++ token.sh
@@ -0,0 +1,11 @@
+Codes: https://github.com/dkbroyt/test/blob/main/test.sh
+
+git clone https://github.com/dkbroyt
+
+cd name.sh
+
+chmod +x name.sh
+
+. /name.sh
+
+common: git add . && git commit -m "feat: initiated the project" && git push origin main
